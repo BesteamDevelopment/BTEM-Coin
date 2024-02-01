1ce753f9ebfe0b403036ad21e9654d38cf7139cd
Giuliano <95681714+GiuBlockchainDEV@users.noreply.github.com>
2024-02-01 13:52:46 +0100

    Update besteamDAO.sol

--- DAO/besteamDAO.sol
+++ DAO/besteamDAO.sol
@@ -21,45 +21,155 @@ M#########M                                                        MMMMMMMMMMM M
 
 pragma solidity ^0.8.20;
 import "./interface.sol";
-import "./contract.sol";
 
-contract besteamDAO is Ownable, ReentrancyGuard {
+contract besteamDAO is Ownable, ReentrancyGuard, Pausable {
 
-    IERC20 public tokenBesteam;
-    
-    constructor(address _tokenBesteam) {
-        tokenBesteam = IERC20(_tokenBesteam);
+    // MANAGER
+    mapping(address => bool) public contractManagers;
+    event ContractManager(address indexed manager, bool status);
+
+    modifier onlyOwnerOrContractManager() {
+        require(msg.sender == owner() || contractManagers[msg.sender], "Caller is not authorized");
+        _;
     }
 
-    //ROLE ____________________________________________________________________________________________________________________________________________________________________________________________________
+    function manageContractManager(address _manager, bool _status) public onlyOwner {
+        require(_manager != address(0), "Invalid address");
+        contractManagers[_manager] = true;
+        emit ContractManager(_manager, _status);
+    }
 
+    // SECURITY
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function transferAnyNewERC20Token(address _tokenAddr, address _to, uint _amount) public onlyOwner {  
+        require(NewIERC20(_tokenAddr).transfer(_to, _amount), "Could not transfer out tokens!");
+    }
+
+    function transferAnyOldERC20Token(address _tokenAddr, address _to, uint _amount) public onlyOwner {    
+        OldIERC20(_tokenAddr).transfer(_to, _amount);
+    }
+
+    receive() external payable {}
+
+    function withdraw() public onlyOwner {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "No GAS balance to withdraw");
+        (bool success, ) = owner().call{value: balance}("");
+        require(success, "GAS withdrawal failed");
+    }
+
+    // DAO TOKEN
+    IERC20 public tokenBesteam;
+    uint256 public besteamDecimals;
+
+    function setBesteamAddress(address _tokenBesteam, uint256 _besteamDecimals) public onlyOwnerOrContractManager {
+        tokenBesteam = IERC20(_tokenBesteam);
+        besteamDecimals = _besteamDecimals;
+    }
+
+    function getBesteamBalance(address _userAddress) public view returns (uint256) {
+        return tokenBesteam.balanceOf(_userAddress);
+    }
+
+    // ROLE DAO
     enum Role { None, Player, President, Counselor, Shareholder, Besteam }
-    
+
     struct Member {
         Role role;
         uint256 membershipExpiry;
     }
 
+    struct PendingMember {
+        address addr;
+        Role role;
+        uint256 payment;
+    }
+
     mapping(address => Member) public members;
-    uint256 public  playerCost = 3 ether; // Costo in wei (esempio)
-    uint256 public  presidentCost = 10 ether; // Costo in wei (esempio)
+    PendingMember[] public pendingMembers;
 
-    function setPlayerCost (uint256 _amount, uint256 _decimals) public onlyOwner {
-        playerCost = _amount * (10 ** _decimals);
+    uint256 public  playerCost = 30 * (10 ** 18); // Costo in wei (esempio)
+    uint256 public  presidentCost = 100 * (10 ** 18); // Costo in wei (esempio)
+
+    function setMember (address _userAddress, uint256 _days, Role _role) public onlyOwnerOrContractManager {
+        members[_userAddress] = Member({role: _role, membershipExpiry: block.timestamp + ((1 days) * _days)});
+    }
+    
+    function setPlayerCost (uint256 _amountPlayer, uint256 _amountPresident, uint256 _decimals) public onlyOwner {
+        playerCost = _amountPlayer * (10 ** _decimals);
+        presidentCost = _amountPresident * (10 ** _decimals);
     }
 
-    function setPresidentCost (uint256 _amount, uint256 _decimals) public onlyOwner {
-        presidentCost = _amount * (10 ** _decimals);
+    function joinDAO(Role _roleType) external nonReentrant {
+        uint256 cost = 0;
+        if (_roleType == Role.Player) {
+            cost = playerCost;
+        } else if (_roleType == Role.President) {
+            cost = presidentCost;
+        }
+        require(tokenBesteam.allowance(msg.sender, address(this)) >= cost, "Token allowance too low");
+        require(tokenBesteam.transferFrom(msg.sender, address(this), cost), "Token transfer failed");
+        uint256 memberIndex = getPendingMemberIndex(msg.sender);
+        require(memberIndex == 9999999999, "Member already pending");
+        // Controlla se l'utente è già un membro approvato
+        if (members[msg.sender].role == Role.None) {
+            pendingMembers.push(PendingMember({
+            addr: msg.sender,
+            role: _roleType,
+            payment: cost
+            }));
+        } else {
+            members[msg.sender].role = _roleType;
+            members[msg.sender].membershipExpiry = block.timestamp + 365 days;
+        }
     }
 
-    function joinAsPlayer() external payable nonReentrant {
-        require(msg.value >= playerCost, "Insufficient funds");
-        members[msg.sender] = Member({role: Role.Player, membershipExpiry: block.timestamp + 365 days});
+    function getPendingMemberIndex(address memberAddress) public view returns (uint256) {
+        for (uint256 i = 0; i < pendingMembers.length; i++) {
+            if (pendingMembers[i].addr == memberAddress) {
+                return uint256(i);
+            }
+        }
+        return 9999999999; // Ritorna 9999999999 se il membro non è trovato
     }
 
-    function joinAsPresident() external payable nonReentrant {
-        require(msg.value >= presidentCost, "Insufficient funds");
-        members[msg.sender] = Member({role: Role.President, membershipExpiry: block.timestamp + 365 days});
+
+    function approveMember(address memberAddress, bool approved) external onlyOwnerOrContractManager {
+        uint256 index = getPendingMemberIndex(memberAddress);
+        require(index != 9999999999, "Member not found in pending list");
+        PendingMember memory pendingMember = pendingMembers[index];
+        if (approved) {
+            // Trasferisci i token al wallet della compagnia
+            require(tokenBesteam.transfer(owner(), pendingMember.payment), "Token transfer to company failed");
+            members[pendingMember.addr] = Member({
+                role: pendingMember.role,
+                membershipExpiry: block.timestamp + 365 days
+            });
+        } else {
+            // Rimborso del 95% dei token
+            uint256 refundAmount = pendingMember.payment * 95 / 100;
+            require(tokenBesteam.transfer(pendingMember.addr, refundAmount), "Token refund failed");
+        }
+        // Rimuovi il membro dalla lista dei pending
+        removePendingMember(index);
+    }
+
+
+    function removePendingMember(uint256 index) internal {
+        require(index < pendingMembers.length, "Invalid index");
+
+        // Shifta tutti gli elementi dopo l'indice verso sinistra di uno
+        for (uint i = index; i < pendingMembers.length - 1; i++) {
+            pendingMembers[i] = pendingMembers[i + 1];
+        }
+        pendingMembers.pop();
     }
     
     function checkMembershipStatus(address user) external view returns (Role, bool) {
@@ -67,113 +177,176 @@ contract besteamDAO is Ownable, ReentrancyGuard {
         return (member.role, member.membershipExpiry > block.timestamp);
     }
 
-    //ELECTION ________________________________________________________________________________________________________________________________________________________________________________________________
-
+    // ELECTION
     struct Election {
-        uint256 startTime;
+        uint256 id;
         uint256 endTime;
         bool isActive;
         mapping(address => uint256) votes;
+        uint256 maxCandidates;
         address[] candidates;
         bool isCandidateRegistrationOpen;
     }
 
+    mapping(address => mapping(uint256 => bool)) public alreadyVoted;
+    mapping(address => mapping(uint256 => bool)) public isAlreadyACandidate;
     Election public currentElection;
+    uint256 public nextElectionId;
 
-    // Funzione per avviare la registrazione dei candidati
-    function openCandidateRegistration() public onlyOwner {
+    function openCandidateRegistration(uint256 _maxCandidates) public onlyOwner {
         require(!currentElection.isActive, "An election is already active");
+        currentElection.maxCandidates = _maxCandidates;
         currentElection.isCandidateRegistrationOpen = true;
+        currentElection.id = nextElectionId;
+        nextElectionId += 1;
     }
 
-    // Funzione per chiudere la registrazione dei candidati e avviare l'elezione
+    function registerAsCandidate() public nonReentrant {
+        require(currentElection.isCandidateRegistrationOpen, "Candidate registration is not open");
+        require(currentElection.maxCandidates > 0, "No available slots");
+        require(members[msg.sender].role == Role.Player || members[msg.sender].role == Role.President, "Not eligible to be a candidate");
+        require(!isAlreadyACandidate[msg.sender][currentElection.id], "Member already registered as a candidate");
+        currentElection.candidates.push(msg.sender);
+        isAlreadyACandidate[msg.sender][currentElection.id] = true;
+        currentElection.maxCandidates -= 1;
+    }
+
     function closeCandidateRegistrationAndStartElection(uint256 _durationInDays) public onlyOwner {
         require(currentElection.isCandidateRegistrationOpen, "Candidate registration is not open");
-
         currentElection.isCandidateRegistrationOpen = false;
-        currentElection.startTime = block.timestamp;
-        currentElection.endTime = block.timestamp + (_durationInDays * 1 days); 
+        currentElection.endTime = block.timestamp + (_durationInDays * 1 days);
         currentElection.isActive = true;
     }
 
-    // Funzione per permettere ai membri di candidarsi
-    function registerAsCandidate() public nonReentrant {
-        require(members[msg.sender].role >= Role.Player, "Not eligible to be a candidate");
-        require(currentElection.isCandidateRegistrationOpen, "Candidate registration is not open");
-
-        // Assicurati che l'utente non sia già un candidato
-        for (uint256 i = 0; i < currentElection.candidates.length; i++) {
-            require(currentElection.candidates[i] != msg.sender, "Already registered as a candidate");
-        }
-
-        currentElection.candidates.push(msg.sender);
-    }
-
-    // Funzione per votare in una elezione
     function voteForCounselor(address _candidate) public nonReentrant {
         require(members[msg.sender].role >= Role.Player, "Not eligible to vote");
         require(currentElection.isActive, "No active election");
         require(block.timestamp < currentElection.endTime, "Election has ended");
         
-        // Assicurati che l'indirizzo per cui si vota sia un candidato valido
-        bool isValidCandidate = false;
-        for (uint256 i = 0; i < currentElection.candidates.length; i++) {
-            if (currentElection.candidates[i] == _candidate) {
-                isValidCandidate = true;
-                break;
-            }
-        }
-        require(isValidCandidate, "Invalid candidate");
-
+        require(!alreadyVoted[msg.sender][currentElection.id], "Already voted");
+        require(isAlreadyACandidate[msg.sender][currentElection.id], "Invalid candidate");
         currentElection.votes[_candidate] += 1;
+        alreadyVoted[msg.sender][currentElection.id] = true;
     }
 
-    //PROPOSAL ________________________________________________________________________________________________________________________________________________________________________________________________
+    function getElectionResults() public view returns (address[] memory, uint256[] memory) {
+        require(!currentElection.isActive, "Election is still active");
+        address[] memory candidates = currentElection.candidates;
+        uint256[] memory votes = new uint256[](candidates.length);
+        for (uint256 i = 0; i < candidates.length; i++) {
+            votes[i] = currentElection.votes[candidates[i]];
+        }
+        return (candidates, votes);
+    }
 
+    //PROPOSAL
     struct Proposal {
-        string description;
-        uint256 deadline;
-        uint voteCount;
-        mapping(address => bool) voted;
+        string description; // Descrizione generale della proposta
+        string[10] optionDescriptions; // Descrizioni delle 10 opzioni di voto
+        uint256 deadline; // Data e ora di scadenza della proposta
+        uint256[10] voteCounts; // Conteggio dei voti per ciascuna opzione
+        uint256 roleType; // Ruolo richiesto per votare su questa proposta (1 Sondaggi Collettivi - 2 Votazioni nominali)
+        uint256 quorum;
+        mapping(address => bool) voted; // Tracciamento di chi ha già votato
     }
 
-    // Array per conservare gli ID delle proposte
-    uint256[] private proposalIds;
-    uint256 private nextProposalId = 0;
-    // Mapping per conservare le proposte utilizzando un ID
-    mapping(uint256 => Proposal) private proposals;
-    // Eventi
+    uint256 public nextProposalId = 0; // ID per la prossima proposta
+    mapping(uint256 => Proposal) private proposals; // Mapping delle proposte
+    mapping(uint256 => bool) public isApproved;
     event NewProposal(uint256 indexed proposalId, string description, uint256 deadline);
     event Voted(uint256 indexed proposalId, address voter);
     
-    // Funzione per creare una nuova proposta di voto
-    function createProposal(string memory _description, uint256 _durationInDays) public onlyOwner {
-        require(members[msg.sender].role == Role.Besteam, "Not a DAO Admin");
-        uint256 proposalId = nextProposalId++;
+    function createProposal(string memory _description, string[10] memory _optionDescriptions, uint256 _durationInDays, uint256 _roleType, uint256 _quorum) public nonReentrant {
+        if(_roleType == 1) {require(members[msg.sender].role == Role.Besteam, "Not a DAO Admin");} 
+        else if (_roleType == 2) {require(members[msg.sender].role == Role.Besteam || members[msg.sender].role == Role.Counselor || members[msg.sender].role == Role.Shareholder, "Not a DAO Admin");} 
+        else {revert("Invalid role type for proposal");}
+        uint256 proposalId = nextProposalId;
         Proposal storage proposal = proposals[proposalId];
         proposal.description = _description;
+        for (uint i = 0; i < _optionDescriptions.length; i++) {
+            proposal.optionDescriptions[i] = _optionDescriptions[i];
+        }
         proposal.deadline = block.timestamp + (_durationInDays * 1 days);
-        proposal.voteCount = 0;
-
-        proposalIds.push(proposalId);
+        proposal.roleType = _roleType;
+        proposal.quorum = _quorum; 
         emit NewProposal(proposalId, _description, proposal.deadline);
+        nextProposalId++;
     }
-
-    // Funzione per votare su una proposta
-    function voteOnProposal(uint256 _proposalId) public {
-        require(members[msg.sender].role != Role.None, "Not a DAO member");
+    
+    function voteOnProposal(uint256 _proposalId, uint256 _optionIndex) public nonReentrant {
+        require(_proposalId < nextProposalId, "Invalid proposal ID");
+        require(_optionIndex < 10, "Invalid option index");
+        require(members[msg.sender].role != Role.Besteam, "DAO Admin");
         Proposal storage proposal = proposals[_proposalId];
         require(block.timestamp < proposal.deadline, "Voting has ended for this proposal");
         require(!proposal.voted[msg.sender], "Already voted");
-
+        uint256 voteWeight = 1;
+        if (proposal.roleType == 1) {
+            require(members[msg.sender].role == Role.Player || members[msg.sender].role == Role.President, "Not a Player or President");
+            if (members[msg.sender].role == Role.President){
+                voteWeight = 3;
+            }
+        } else if (proposal.roleType == 2) {
+            require(members[msg.sender].role == Role.Counselor || members[msg.sender].role == Role.Shareholder, "Not a Counselor or Shareholder");
+        } else {
+            revert("Not eligible to vote on this proposal");
+        }
         proposal.voted[msg.sender] = true;
-        proposal.voteCount += 1;
-
+        proposal.voteCounts[_optionIndex] += voteWeight;
         emit Voted(_proposalId, msg.sender);
     }
 
-    // Funzione per ottenere il conteggio dei voti di una proposta
-    function getVoteCount(uint256 _proposalId) public view returns (uint256) {
-        return proposals[_proposalId].voteCount;
+    function getProposalOptionsAndVoteCounts(uint256 _proposalId) public view returns (string[10] memory, uint256[10] memory, bool) {
+        require(_proposalId < nextProposalId, "Invalid proposal ID");
+        Proposal storage proposal = proposals[_proposalId];
+        uint256 totalVotes = 0;
+        bool quorumReached;
+        for (uint256 i = 0; i < proposal.voteCounts.length; i++) {
+            totalVotes += proposal.voteCounts[i];
+        }
+        if (totalVotes >= proposal.quorum) {
+            quorumReached = true;
+        } else {
+            quorumReached = false;
+        }
+        return (proposal.optionDescriptions, proposal.voteCounts, quorumReached);
     }
+
+    function getActiveProposalsInRange(uint256 startIndex, uint256 endIndex) public view returns (uint256[] memory) {
+        require(startIndex < endIndex, "Start index must be less than end index");
+        require(endIndex <= nextProposalId, "End index out of bounds");
+        uint256 activeCount = 0;
+        // Determina il numero di proposte attive nell'intervallo
+        for (uint256 i = startIndex; i < endIndex; i++) {
+            if (proposals[i].deadline > block.timestamp) {
+                activeCount++;
+            }
+        }
+        // Raccogli gli ID delle proposte attive nell'intervallo
+        uint256[] memory activeProposals = new uint256[](activeCount);
+        uint256 currentIndex = 0;
+        for (uint256 i = startIndex; i < endIndex; i++) {
+            if (proposals[i].deadline > block.timestamp) {
+                activeProposals[currentIndex] = i;
+                currentIndex++;
+            }
+        }
+        return activeProposals;
+    }
+
+    function checkProposalOutcome(uint256 _proposalId) public {
+        require(_proposalId < nextProposalId, "Invalid proposal ID");
+        Proposal storage proposal = proposals[_proposalId];
+        require(block.timestamp > proposal.deadline, "Voting is still active");
+        uint256 totalVotes = 0;
+        for (uint256 i = 0; i < proposal.voteCounts.length; i++) {
+            totalVotes += proposal.voteCounts[i];
+        }
+        if (totalVotes >= proposal.quorum) {
+            isApproved[_proposalId] = true;
+        } else {
+            isApproved[_proposalId] = false;
+        }
+    }
+
 }
